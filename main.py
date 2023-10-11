eab10e81664f4a6c0f8d33b4ebedc497a23d7502
Frank E <74784382+feyaif3@users.noreply.github.com>
2023-10-11 21:05:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ root.title("Minesweeper")
 root.resizable(False, False)
 
 #Framing the window
-
 top_frame = Frame(root, bg="red", width=1440, height=180)
 top_frame.place(x=0, y=0)
 
+left_frame = Frame(root, bg="blue", width=360, height=540)
+left_frame.place(x=0, y=180)
 
 
 
