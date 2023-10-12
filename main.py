7f89533db615e71e75bd4bdb2b6ee0a4b413a864
Frank E <74784382+feyaif3@users.noreply.github.com>
2023-10-12 20:16:43 +0100

    all frames created

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from tkinter import *
+import settings
 
 # Create the root window
 root = Tk()
 root.configure(bg="black")
-root.geometry("1440x720")
+root.geometry(f"{settings.WIDTH}x{settings.HEIGHT}")
 root.title("Minesweeper")
 root.resizable(False, False)
 
 #Framing the window
-top_frame = Frame(root, bg="red", width=1440, height=180)
+top_frame = Frame(root, bg="red", width=settings.WIDTH, height=settings.height_percentage(25))
 top_frame.place(x=0, y=0)
 
-left_frame = Frame(root, bg="blue", width=360, height=540)
-left_frame.place(x=0, y=180)
+left_frame = Frame(root, bg="blue", width=settings.width_percentage(25), height=settings.height_percentage(75))
+left_frame.place(x=0, y=settings.height_percentage(25))
 
-
-
+game_frame = Frame(root, bg="green", width=settings.width_percentage(75), height=settings.height_percentage(75))
+game_frame.place(x=settings.width_percentage(25), y=settings.height_percentage(25))
 
 #Run the window
 root.mainloop()
---
+++ settings.py
@@ -0,0 +1,9 @@
+
+WIDTH = 1440
+HEIGHT = 720
+
+def height_percentage(percentage):
+    return (HEIGHT / 100) * percentage
+
+def width_percentage(percentage):
+    return (WIDTH / 100) * percentage
